4e3f2f0ba8ca185391c76a30ada0a35c7e475373
camic <camic@CamiColpitts.dhcp.wustl.edu>
2024-10-02 14:07:51 -0500

    Done!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(222, 100, 166);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.22);
+		
+		StdDraw.setPenColor(255, 185, 81);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.1);
+		
+		
+		/*StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);*/
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,31 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double semiMajorAxis = in.nextDouble();
+	
+		
+		//System.out.println(shapeType + redComponent + greenComponent + blueComponent + isFilled + parameterOne + parameterTwo + parameterThree);
+		
+		Color pink = new Color (redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(Color.pink);
+
+		if (shapeType.equals("rectangle")) {
+			StdDraw.filledSquare(parameterOne, parameterTwo, parameterThree);
+		}
+		
+		if (shapeType.equals("ellipse")) {
+			StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, semiMajorAxis);
+		}
+		
 		
 	}
 }
